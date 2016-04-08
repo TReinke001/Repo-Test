743176e6ec5eecbdda7dfe5aabb40bdbc49ade04
TReinke001 <tylerreinke@hotmail.com>
2016-04-08 16:46:15 -0700

    added script and scene

---
+++ Assets/REPOTESTSCRIPT.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class REPOTESTSCRIPT : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+		print ("TESTED TEST TEST TEST");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
